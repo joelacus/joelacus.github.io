ee1e7340c4054348733877bc910c94e2601bd679
J <43767965+joelacus@users.noreply.github.com>
2021-06-09 21:13:09 +0000

    Update colours-main.js

--- customhomepage/js/colours-main.js
+++ customhomepage/js/colours-main.js
@@ -28,184 +28,210 @@ function is3DColour(){
 
 // Change background colour button
 $(document).on( "click", ".btn-bg-colour-grey3", function(e) {
-	$('body').attr('style', 'background: '+grey3);
+	$('.background-style').attr('style', 'background: '+grey3);
 	window.backgroundColour = grey3
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-grey2", function(e) {
-	$('body').attr('style', 'background: '+grey2);
+	$('.background-style').attr('style', 'background: '+grey2);
 	window.backgroundColour = grey2
 	is3DColour();
 	window.isPhotoBackground = false
 });        	
 $(document).on( "click", ".btn-bg-colour-red", function(e) {
-	$('body').attr('style', 'background: '+red);       		
+	$('.background-style').attr('style', 'background: '+red);
 	window.backgroundColour = red
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-green", function(e) {
-	$('body').attr('style', 'background: '+green); 
+	$('.background-style').attr('style', 'background: '+green); 
 	window.backgroundColour = green
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-blue", function(e) {
-	$('body').attr('style', 'background: '+blue); 
+	$('.background-style').attr('style', 'background: '+blue); 
 	window.backgroundColour = blue
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-blue2", function(e) {
-	$('body').attr('style', 'background: '+blue2); 
+	$('.background-style').attr('style', 'background: '+blue2); 
 	window.backgroundColour = blue2
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-white2", function(e) {
-	$('body').attr('style', 'background: '+white2); 
+	$('.background-style').attr('style', 'background: '+white2); 
 	window.backgroundColour = white2
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-yellow", function(e) {
-	$('body').attr('style', 'background: '+yellow); 
+	$('.background-style').attr('style', 'background: '+yellow); 
 	window.backgroundColour = yellow
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-purple", function(e) {
-	$('body').attr('style', 'background: '+purple);
+	$('.background-style').attr('style', 'background: '+purple);
 	window.backgroundColour = purple
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-teal", function(e) {
-	$('body').attr('style', 'background: '+teal); 
+	$('.background-style').attr('style', 'background: '+teal); 
 	window.backgroundColour = teal
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-orange", function(e) {
-	$('body').attr('style', 'background: '+orange); 
+	$('.background-style').attr('style', 'background: '+orange); 
 	window.backgroundColour = orange
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-pink", function(e) {
-	$('body').attr('style', 'background: '+pink); 
+	$('.background-style').attr('style', 'background: '+pink); 
 	window.backgroundColour = pink
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-green2", function(e) {
-	$('body').attr('style', 'background: '+green2); 
+	$('.background-style').attr('style', 'background: '+green2); 
 	window.backgroundColour = green2
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-green3", function(e) {
-	$('body').attr('style', 'background: '+green3); 
+	$('.background-style').attr('style', 'background: '+green3); 
 	window.backgroundColour = green3
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-purple2", function(e) {
-	$('body').attr('style', 'background: '+purple2); 
+	$('.background-style').attr('style', 'background: '+purple2); 
 	window.backgroundColour = purple2
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-white", function(e) {
-	$('body').attr('style', 'background: '+white); 
+	$('.background-style').attr('style', 'background: '+white); 
 	window.backgroundColour = white
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-grey", function(e) {
-	$('body').attr('style', 'background: '+grey); 
+	$('.background-style').attr('style', 'background: '+grey); 
 	window.backgroundColour = grey
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-cyan", function(e) {
-	$('body').attr('style', 'background: '+cyan); 
+	$('.background-style').attr('style', 'background: '+cyan); 
 	window.backgroundColour = cyan
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-grey4", function(e) {
-	$('body').attr('style', 'background: '+grey4); 
+	$('.background-style').attr('style', 'background: '+grey4); 
 	window.backgroundColour = grey4
 	is3DColour();
 	window.isPhotoBackground = false
 });
 $(document).on( "click", ".btn-bg-colour-purple3", function(e) {
-	$('body').attr('style', 'background: '+purple3); 
+	$('.background-style').attr('style', 'background: '+purple3); 
 	window.backgroundColour = purple3
 	is3DColour();
 	window.isPhotoBackground = false
 });
 
 // Change widget colour button
+function setOpacitySlider(){
+	$('#widget-colour-opacity-slider').val(100)
+    var output = document.getElementById("widget-colour-opacity-value");
+	output.innerHTML = '100%';
+}
 $(document).on( "click", ".btn-colour-grey3", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", grey3).css("border", "solid 1px "+grey3);
+    $(activeWidget).css("background-color", grey3);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-grey2", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", grey2).css("border", "solid 1px "+grey2);
+    $(activeWidget).css("background-color", grey2);
+    setOpacitySlider()
 });         
 $(document).on( "click", ".btn-colour-red", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", red).css("border", "solid 1px "+red);
+    $(activeWidget).css("background-color", red);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-green", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", green).css("border", "solid 1px "+green);
+    $(activeWidget).css("background-color", green);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-blue", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", blue).css("border", "solid 1px "+blue);
+    $(activeWidget).css("background-color", blue);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-blue2", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", blue2).css("border", "solid 1px "+blue2);
+    $(activeWidget).css("background-color", blue2);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-white2", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", white2).css("border", "solid 1px "+white2);
+    $(activeWidget).css("background-color", white2);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-yellow", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", yellow).css("border", "solid 1px "+yellow);
+    $(activeWidget).css("background-color", yellow);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-purple", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", purple).css("border", "solid 1px "+purple);
+    $(activeWidget).css("background-color", purple);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-teal", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", teal).css("border", "solid 1px "+teal);
+    $(activeWidget).css("background-color", teal);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-orange", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", orange).css("border", "solid 1px "+orange);
+    $(activeWidget).css("background-color", orange);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-pink", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", pink).css("border", "solid 1px "+pink);
+	$(activeWidget).css("background-color", pink);
+	setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-green2", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", green2).css("border", "solid 1px "+green2);
+    $(activeWidget).css("background-color", green2);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-green3", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", green3).css("border", "solid 1px "+green3);
+    $(activeWidget).css("background-color", green3);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-purple2", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", purple2).css("border", "solid 1px "+purple2);
+    $(activeWidget).css("background-color", purple2);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-white", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", white).css("border", "solid 1px "+white);
+    $(activeWidget).css("background-color", white);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-grey", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", grey).css("border", "solid 1px "+grey);
+    $(activeWidget).css("background-color", grey);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-cyan", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", cyan).css("border", "solid 1px "+cyan);
+    $(activeWidget).css("background-color", cyan);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-grey4", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", grey4).css("border", "solid 1px "+grey4);
+    $(activeWidget).css("background-color", grey4);
+    setOpacitySlider()
 });
 $(document).on( "click", ".btn-colour-purple3", function(e) {
-    $(this).parent().parent().parent().parent().parent().css("background-color", purple3).css("border", "solid 1px "+purple3);
+    $(activeWidget).css("background-color", purple3);
+    setOpacitySlider()
+    
 });
